3081f795aab68d6254880f9bbf0da6253d0e0c99
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-29 21:01:20 +0300

    0.0.01.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 import dotenv from 'dotenv';
+
+dotenv.config();
 
 if (!process.env.BOT_TOKEN) {
   throw new Error('BOT_TOKEN не найден в .env файле!');
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on(message('voice'), async ctx => {
+  await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+});
+
+bot.command('start', async (ctx) =>{
+  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
 
 bot.launch();
 
