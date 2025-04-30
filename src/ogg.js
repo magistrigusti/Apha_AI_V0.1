56e7b81e81efbcc7451f63e540ff26ca20312482
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-30 07:28:56 +0300

    0.1.04

--- src/ogg.js
+++ src/ogg.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import ffmpeg from 'fluent-ffmpeg';
+import installer from '@ffmpeg-installer/ffmpeg';
 import { createWriteStream } from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
@@ -6,13 +8,23 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 class OggConverter {
-  constructor() {}
+  constructor() {
+    ffmpeg.setFfmpegPath(installer.path);
+  }
 
   toMp3(input, output) {
     try {
-
+      const outputPath = resolve(dirname(input), `${output}.mp3`);
+      return new Promise((resolve,reject) => {
+        ffmpeg(input)
+          .inputOption('-t 30')
+          .output(outputPath)
+          .on('end', () => resolve(outputPath))
+          .on('error', err => reject(err.message))
+          .run()
+      });
     } catch (e) {
-      console.log('Error while creating ogg', e.message);
+      console.log('Error while creating mp3', e.message);
     }
   }
 
@@ -22,7 +34,7 @@ class OggConverter {
       const response = await axios({
         method: 'get',
         url,
-        responseType: 'streem',
+        responseType: 'stream',
       });
 
       return new Promise(resolve => {
